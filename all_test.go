a813aec69e099069f4c39e5825ea090f7c69d87f
Farbod Salimi <4585739+farbodsalimi@users.noreply.github.com>
2022-08-19 16:58:51 -0700

    test: test parallelization

--- all_test.go
+++ all_test.go
@@ -7,141 +7,190 @@ import (
 )
 
 func TestAllInt(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name   string
 		input  []int
 		expect bool
 	}{
 		{
+			name:   "Empty slice",
 			input:  []int{},
 			expect: true,
 		},
 		{
+			name:   "Valid slice",
 			input:  []int{3, 4, 5, 1, 2},
 			expect: true,
 		},
 		{
+			name:   "One zero element",
 			input:  []int{3, 0, 1, 2},
 			expect: false,
 		},
 		{
+			name:   "Two zero elements",
 			input:  []int{0, -1, 0, -2},
 			expect: false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, All(testCase.input))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, All(testCase.input))
+		})
 	}
 }
 
 func TestAllUint(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name   string
 		input  []uint
 		expect bool
 	}{
 		{
+			name:   "Empty slice",
 			input:  []uint{},
 			expect: true,
 		},
 		{
+			name:   "Valid slice",
 			input:  []uint{3, 4, 5, 1, 2},
 			expect: true,
 		},
 		{
+			name:   "One zero element",
 			input:  []uint{3, 0, 1, 2},
 			expect: false,
 		},
 		{
+			name:   "Two zero elements",
 			input:  []uint{0, 1, 0, 2},
 			expect: false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, All(testCase.input))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, All(testCase.input))
+		})
 	}
 }
 
 func TestAllFloat32(t *testing.T) {
 	testCases := []struct {
+		name   string
 		input  []float32
 		expect bool
 	}{
 		{
+			name:   "Empty slice",
 			input:  []float32{},
 			expect: true,
 		},
 		{
+			name:   "Valid slice",
 			input:  []float32{3.1, 4.1, 5.1, 1.1, 2.1},
 			expect: true,
 		},
 		{
+			name:   "Two zero elements",
 			input:  []float32{0.1, 0.0, 0, 2.1},
 			expect: false,
 		},
 		{
-			input:  []float32{0, -1.111, -0.0, -2.222},
+			name:   "One negative zero element",
+			input:  []float32{-1.111, -0.0, -2.222},
 			expect: false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, All(testCase.input))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, All(testCase.input))
+		})
 	}
 }
 
 func TestAllFloat64(t *testing.T) {
 	testCases := []struct {
+		name   string
 		input  []float64
 		expect bool
 	}{
 		{
+			name:   "Empty slice",
 			input:  []float64{},
 			expect: true,
 		},
 		{
+			name:   "Valid slice",
 			input:  []float64{3.1, 4.1, 5.1, 1.1, 2.1},
 			expect: true,
 		},
 		{
+			name:   "Two zero elements",
 			input:  []float64{0.1, 0.0, 0, 2.1},
 			expect: false,
 		},
 		{
-			input:  []float64{0, -1.111, -0.0, -2.222},
+			name:   "One negative zero element",
+			input:  []float64{-1.111, -0.0, -2.222},
 			expect: false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, All(testCase.input))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, All(testCase.input))
+		})
 	}
 }
 
 func TestAllString(t *testing.T) {
 	testCases := []struct {
+		name   string
 		input  []string
 		expect bool
 	}{
 		{
+			name:   "Empty slice",
 			input:  []string{},
 			expect: true,
 		},
 		{
+			name:   "Valid slice",
 			input:  []string{"a", "b", "c", "d"},
 			expect: true,
 		},
 		{
+			name:   "All elements are empty string",
 			input:  []string{"", "", string(""), " "},
 			expect: false,
 		},
 		{
+			name:   "Two empty string elements",
 			input:  []string{"a", "", "c", ""},
 			expect: false,
 		},
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, All(testCase.input))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, All(testCase.input))
+		})
 	}
 }
--- join_test.go
+++ join_test.go
@@ -7,21 +7,27 @@ import (
 )
 
 func TestJoinInt(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name      string
 		input     []int
 		delimiter string
 		expect    string
 	}{
 		{
+			name:   "Empty slice",
 			input:  []int{},
 			expect: "",
 		},
 		{
+			name:      "Empty delimiter",
 			input:     []int{1, 2, 3},
 			delimiter: "",
 			expect:    "123",
 		},
 		{
+			name:      "Comma delimiter",
 			input:     []int{1, 2, 3, 1, 2, 3},
 			delimiter: ",",
 			expect:    "1,2,3,1,2,3",
@@ -29,26 +35,36 @@ func TestJoinInt(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		})
 	}
 }
 
 func TestJoinUint(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name      string
 		input     []uint
 		delimiter string
 		expect    string
 	}{
 		{
+			name:   "Empty slice",
 			input:  []uint{},
 			expect: "",
 		},
 		{
+			name:      "Empty delimiter",
 			input:     []uint{1, 2, 3},
 			delimiter: "",
 			expect:    "123",
 		},
 		{
+			name:      "Comma delimiter",
 			input:     []uint{1, 2, 3, 1, 2, 3},
 			delimiter: ",",
 			expect:    "1,2,3,1,2,3",
@@ -56,26 +72,36 @@ func TestJoinUint(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		})
 	}
 }
 
 func TestJoinFloat32(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name      string
 		input     []float32
 		delimiter string
 		expect    string
 	}{
 		{
+			name:   "Empty slice",
 			input:  []float32{},
 			expect: "",
 		},
 		{
+			name:      "Empty delimiter",
 			input:     []float32{1.1, 2.1, 3.1},
 			delimiter: "",
 			expect:    "1.12.13.1",
 		},
 		{
+			name:      "Comma delimiter",
 			input:     []float32{1.1, 2.1, 3.1, 1.1, 2.1, 3.1},
 			delimiter: ",",
 			expect:    "1.1,2.1,3.1,1.1,2.1,3.1",
@@ -83,26 +109,36 @@ func TestJoinFloat32(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		})
 	}
 }
 
 func TestJoinFloat64(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name      string
 		input     []float64
 		delimiter string
 		expect    string
 	}{
 		{
+			name:   "Empty slice",
 			input:  []float64{},
 			expect: "",
 		},
 		{
+			name:      "Empty delimiter",
 			input:     []float64{1.1, 2.1, 3.1},
 			delimiter: "",
 			expect:    "1.12.13.1",
 		},
 		{
+			name:      "Comma delimiter",
 			input:     []float64{1.1, 2.1, 3.1, 1.1, 2.1, 3.1},
 			delimiter: ",",
 			expect:    "1.1,2.1,3.1,1.1,2.1,3.1",
@@ -110,26 +146,36 @@ func TestJoinFloat64(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		})
 	}
 }
 
 func TestJoinString(t *testing.T) {
+	t.Parallel()
+
 	testCases := []struct {
+		name      string
 		input     []string
 		delimiter string
 		expect    string
 	}{
 		{
+			name:   "Empty slice",
 			input:  []string{},
 			expect: "",
 		},
 		{
+			name:      "Empty delimiter",
 			input:     []string{"a", "b", "c"},
 			delimiter: "",
 			expect:    "abc",
 		},
 		{
+			name:      "Comma delimiter",
 			input:     []string{"a", "b", "c", "d"},
 			delimiter: ",",
 			expect:    "a,b,c,d",
@@ -137,6 +183,10 @@ func TestJoinString(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, testCase.expect, Join(testCase.input, testCase.delimiter))
+		})
 	}
 }
