b4286b2eccb94ccfdd7cefbc699f59d596988518
Farbod Salimi <4585739+farbodsalimi@users.noreply.github.com>
2025-08-21 18:44:25 -0700

    test: zip func

--- zip_test.go
+++ zip_test.go
@@ -16,7 +16,7 @@ func TestZip(t *testing.T) {
 		expect []Pair[int, string]
 	}{
 		{
-			name:   "Empty slices",
+			name:   "Same length slices",
 			input1: []int{1, 2, 3},
 			input2: []string{"a", "b", "c"},
 			expect: []Pair[int, string]{
@@ -24,6 +24,48 @@ func TestZip(t *testing.T) {
 				{First: 2, Second: "b"},
 				{First: 3, Second: "c"},
 			},
+		},
+		{
+			name:   "Empty slices",
+			input1: []int{},
+			input2: []string{},
+			expect: []Pair[int, string]{},
+		},
+		{
+			name:   "Different lengths - input1 shorter",
+			input1: []int{1, 2},
+			input2: []string{"a", "b", "c"},
+			expect: []Pair[int, string]{
+				{First: 1, Second: "a"},
+				{First: 2, Second: "b"},
+			},
+		},
+		{
+			name:   "Different lengths - input2 shorter",
+			input1: []int{1, 2, 3},
+			input2: []string{"a", "b"},
+			expect: []Pair[int, string]{
+				{First: 1, Second: "a"},
+				{First: 2, Second: "b"},
+			},
+		},
+		{
+			name:   "Nil slices",
+			input1: nil,
+			input2: nil,
+			expect: []Pair[int, string]{},
+		},
+		{
+			name:   "Nil input1",
+			input1: nil,
+			input2: []string{"a", "b", "c"},
+			expect: []Pair[int, string]{},
+		},
+		{
+			name:   "Nil input2",
+			input1: []int{1, 2, 3},
+			input2: nil,
+			expect: []Pair[int, string]{},
 		},
 	}
 
